8c3c46cfc5144ef1cb6bc048b970cadae947481d
DanGiaime <dsg6874@g.rit.edu>
2017-11-05 01:13:42 -0500

    GameManager

--- React-Shawn/Assets/Scripts/GameManager.cs
+++ React-Shawn/Assets/Scripts/GameManager.cs
@@ -6,10 +6,19 @@ public class GameManager : MonoBehaviour {
 
 	public float score;
 	public int lives;
+	public GameObject leftArrow;
+	public GameObject rightArrow;
+	public GameObject upArrow;
+	public GameObject downArrow;
 
 	// Use this for initialization
 	void Start () {
-		
+
+		//make arrow objects
+		Instantiate (leftArrow, transform);
+		Instantiate (rightArrow, transform);
+		Instantiate (upArrow, transform);
+		Instantiate (downArrow, transform);
 	}
 	
 	// Update is called once per frame
