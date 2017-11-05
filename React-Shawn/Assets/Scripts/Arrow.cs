fec0ed36eb3ede41120caa536581cf60ccb5ee47
DanGiaime <dsg6874@g.rit.edu>
2017-11-05 01:10:47 -0500

    Start arrows and gm

--- React-Shawn/Assets/Scripts/Arrow.cs
+++ React-Shawn/Assets/Scripts/Arrow.cs
@@ -4,13 +4,39 @@ using UnityEngine;
 
 public class Arrow : MonoBehaviour {
 
+	public bool lit;
+	public float lightProb;
+	public KeyCode code;
+	public GameObject gameManager;
+
+	//Tiem until dark
+	public float ttl;
+
 	// Use this for initialization
 	void Start () {
-		
+		this.lit = false;
+		this.gameManager = GameObject.Find ("gameManager");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (!this.lit) {
+			float shouldLight = Random.Range (0f, 1f);
+			if (shouldLight < this.lightProb) {
+				this.lit = true;
+			}
+			if (Input.GetKeyDown (code)) {
+				gameManager.GetComponent<GameManager> ().lives--;
+			}
+		} else if (lit && ttl > 0) {
+			if (Input.GetKeyDown (code)) {
+				gameManager.GetComponent<GameManager> ().score++;
+				lit = false;
+			}
+		} else if (lit && ttl < 0) {
+			gameManager.GetComponent<GameManager> ().lives--;
+			this.lit = false;
+		}
 	}
+
 }
---
+++ React-Shawn/Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public float score;
+	public int lives;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
